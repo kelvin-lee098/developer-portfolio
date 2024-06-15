650b20190c8b43e79f800f0a3dd2a36de1a0cd36
khanhchelsea <khanhduong1203@gmail.com>
2024-06-15 11:49:26 +0700

    r2

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -1,51 +1,53 @@
-import ayla from '/public/image/ayla.jpg';
-import crefin from '/public/image/crefin.jpg';
-import realEstate from '/public/image/real-estate.jpg';
-import travel from '/public/image/travel.jpg';
+import ayla from "/public/image/ayla.jpg";
+import crefin from "/public/image/crefin.jpg";
+import realEstate from "/public/image/real-estate.jpg";
+import travel from "/public/image/travel.jpg";
 
 export const projectsData = [
-    {
-        id: 1,
-        name: 'AI Powered Financial App',
-        description: "Me and my team built an AI-powered financial mobile application. I have developed API using Express, Typescript, OpenAI, AWS, and MongoDB. Used OTP via AWS SES, Google, and Facebook for the authentication system. Built AI assistants using OpenAI's latest model and trained using our dataset. Voice messages are converted to text using AWS Transcribe. The app fetches data from Google Sheets and generates a PDF term sheet, sent via AWS SES.",
-        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
-        role: 'Backend Developer',
-        code: '',
-        demo: '',
-        image: crefin,
-    },
-    {
-        id: 2,
-        name: 'Travel Agency App',
-        description: 'I have designed and developed a full-stack web app for 2Expedition, a travel agency in Armenia. I created the UI using NextJS, Typescript, MUI, TailwindCSS, Google Maps, Sun-Editor, and React Slick. The app supports multiple languages and currencies. I developed the API using NestJS, Typescript, MySQL, TypeORM, AWS, and Nodemailer. I deployed the front-end app to AWS Amplify and the back-end app to AWS EC2.',
-        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
-        role: 'Full Stack Developer',
-        code: '',
-        demo: '',
-        image: travel,
-    },
-    {
-        id: 3,
-        name: 'AI Powered Real Estate',
-        description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
-        tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
-        code: '',
-        role: 'Full Stack Developer',
-        demo: '',
-        image: realEstate,
-    },
-    {
-        id: 4,
-        name: 'On-duty scheduling software',
-        description: "My team and I developed a On-duty scheduling software for Lai Chau hydroelectric plant. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
-        tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
-        code: '',
-        demo: '',
-        image: ayla,
-        role: 'Full Stack Developer',
-    }
+  {
+    id: 1,
+    name: "University ERP",
+    description:
+      "Me and my team built an ERP for University. We have developed app using React, Typescript, Go, AWS, and PostgreSql. Used LDAP for the authentication system.",
+    tools: ["React", "PostgreSql", "Go", "EC2", "Nginx"],
+    role: "Full Stack Developer",
+    code: "",
+    demo: "",
+    image: crefin,
+  },
+  {
+    id: 3,
+    name: "Hosptial On-duty Scheduling",
+    description:
+      "My team built an optimization application for hospital to scheduling on-duty and manage works, tasks, swap shift, ... . We used C++ to develop the core run algorithm. We utilized React, Mantine, Formik, Redux Toolkit, and other npm libraries for the UI and Firebase for notifications between devices includes mobile and web browser. We also develop mobile app via React Native, support both Android and iOS devices.",
+    tools: [
+      "React",
+      "Redux",
+      "Mantine",
+      "React Native",
+      "TypeScript",
+      "Go",
+      "NodeJS",
+      "Cronjob",
+      "JWT",
+    ],
+    code: "",
+    role: "Full Stack Developer",
+    demo: "",
+    image: realEstate,
+  },
+  {
+    id: 4,
+    name: "Hydroelectric Plant on-duty scheduling",
+    description:
+      "My team and I developed a On-duty scheduling software for Lai Chau hydroelectric plant. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+    tools: [".NET", "MSSQL", "React Native", "Redux", "Firebase"],
+    code: "",
+    demo: "",
+    image: ayla,
+    role: "Front-end Developer",
+  },
 ];
-
 
 // Do not remove any property.
 // Leave it blank instead as shown below
